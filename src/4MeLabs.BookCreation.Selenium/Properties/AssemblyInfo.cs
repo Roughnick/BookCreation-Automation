9d2cc0fc527ad92275746cd375a70811a5b1eccc
Roughnick <JGStutson@gmail.com>
2017-10-08 22:16:57 -0600

    Initial Creation of Project. Added Chromedriver and Selenium NuGet packages

---
+++ src/4MeLabs.BookCreation.Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("4MeLabs.BookCreation.Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4MeLabs.BookCreation.Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59e2a63d-efce-45aa-8748-a69a503ad028")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/4MeLabs.BookCreation.Selenium/TestCases/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace _4MeLabs.BookCreation.Selenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver Driver;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            var path = Path.GetFullPath("./");
+            Driver = new ChromeDriver(path);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Driver.Navigate().GoToUrl("http://tohru.4melabs.com/ikigai/books.php");
+            Driver.Quit();
+        }
+    }
+}
