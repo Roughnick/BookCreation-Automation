d916052bd230ca9166ce6a4d3cdcc440edf25612
Roughnick <JGStutson@gmail.com>
2017-10-09 22:05:45 -0600

    Forgot to save chances before committing

--- src/4MeLabs.BookCreation.Selenium/PageObjects/BookPageObjects/BookPage.cs
+++ src/4MeLabs.BookCreation.Selenium/PageObjects/BookPageObjects/BookPage.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace _4MeLabs.BookCreation.Selenium.PageObjects.BookPageObjects
 {
-    class BookPage
+    public class BookPage : PageObject
     {
+        public BookPageElementMap Map => new BookPageElementMap();
+
+        public BookPage(IWebDriver driver) : base(driver) { }
+
+        public void AddNewBook()
+        {
+
+        }
     }
 }
--- src/4MeLabs.BookCreation.Selenium/PageObjects/BookPageObjects/BookPageElementMap.cs
+++ src/4MeLabs.BookCreation.Selenium/PageObjects/BookPageObjects/BookPageElementMap.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace _4MeLabs.BookCreation.Selenium.PageObjects.BookPageObjects
 {
-    class BookPageElementMap
+    public class BookPageElementMap
     {
+        public By NavigationHomeLogo => By.ClassName("navbar-brand");
+        public By AddBookBtn => By.Id("add_book");
+        public By BookTable => By.Id("book_list");
+        public By NewRowAdded => By.XPath("//td[contains(.,'Just Now')]");
     }
 }
