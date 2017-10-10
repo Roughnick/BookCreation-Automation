874de89156181ad25a1a76dcdda977b6df34ecc2
Roughnick <JGStutson@gmail.com>
2017-10-09 22:04:35 -0600

    Started adding page elements from the book page

---
+++ src/4MeLabs.BookCreation.Selenium/PageObjects/BookPageObjects/BookPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4MeLabs.BookCreation.Selenium.PageObjects.BookPageObjects
+{
+    class BookPage
+    {
+    }
+}
---
+++ src/4MeLabs.BookCreation.Selenium/PageObjects/PageObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace _4MeLabs.BookCreation.Selenium.PageObjects
+{
+    public class PageObject
+    {
+        protected IWebDriver _driver;
+
+        protected PageObject(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void GoToUrl(string siteUrl)
+        {
+            _driver.Navigate().GoToUrl(siteUrl);
+        }
+    }
+}
